80b959b3fd4bb6ff4bd1e8e71f6f66d6b6afae88
Andrey Pavlov <couchemar@yandex.ru>
2013-05-20 18:03:53 +0400

    handle turn + tests

--- lib/table/server.ex
+++ lib/table/server.ex
@@ -2,7 +2,10 @@ defmodule Kozel.Table.Server do
   use GenServer.Behaviour
 
   import Kozel.Cards, only: [ produce_cards: 0,
-                              deal: 1 ]
+                              deal: 1,
+                              check_hand: 2,
+                              available_turns: 2,
+                              turn: 4 ]
 
   def start_link() do
     :gen_server.start_link(__MODULE__, [], [])
@@ -10,43 +13,51 @@ defmodule Kozel.Table.Server do
 
   defrecord TableState, round: :waiting_joins,
                         decs: [],
-                        players_by_id: nil,
+                        players_by_token: nil,
                         ids_by_token: nil,
+                        tokens_by_id: nil,
                         hands_by_token: nil,
-                        next_move: 0
+                        next_move: 0,
+                        ready: [],
+                        table: []
 
   def init([]) do
+    :random.seed(:os.timestamp)
     {h1, h2, h3, h4} = deal(produce_cards)
     {:ok, TableState.new(decs: [h1, h2, h3, h4])}
   end
 
-  def handle_call(:join, from,
+  def handle_call(:join, _from,
                   TableState[round: :waiting_joins,
-                             decs: decs,
-                             players_by_id: players,
-                             ids_by_token: ids]=state) do
+                             ids_by_token: ids,
+                             tokens_by_id: tokens]=state) do
     token = :os.timestamp
-    if players == nil do
-      id = 1
-      players = HashDict.new [{id, from}]
-    else
-      id = HashDict.size(players) + 1
-      players = HashDict.put players, id, from
-    end
 
     if ids == nil do
-      tokens = HashDict.new [{token, id}]
+      id = 1
+      ids = HashDict.new [{token, id}]
     else
-      tokens = HashDict.put ids, token, id
+      id = HashDict.size(ids) + 1
+      ids = HashDict.put ids, token, id
     end
 
-    new_state = state.players_by_id(players)
-    {:reply, {:ok, token}, new_state.ids_by_token(tokens)}
+    if tokens == nil do
+      tokens = HashDict.new [{id, token}]
+    else
+      tokens = HashDict.put tokens, id, token
+    end
+
+    new_state = state.ids_by_token(ids)
+    new_state = new_state.tokens_by_id(tokens)
+    if id == 4 do
+      new_state = new_state.next_move(:random.uniform(4))
+    end
+    {:reply, {:ok, token}, new_state}
   end
 
   def handle_call({:get_cards, token}, _from,
                   TableState[decs: decs,
-                            hands_by_token: hands]=state) when length(decs) > 0 do
+                             hands_by_token: hands]=state) when length(decs) > 0 do
     [d|new_decs] = decs
     if hands == nil do
       hands = HashDict.new [{token, d}]
@@ -57,9 +68,84 @@ defmodule Kozel.Table.Server do
     {:reply, d, new_state.hands_by_token(hands)}
   end
 
+  def handle_call({:ready, token}, from,
+                  TableState[ready: ready,
+                             players_by_token: players]=state) when length(ready) < 3 do
+    if players == nil do
+      players = HashDict.new [{token, from}]
+    else
+      players = HashDict.put players, token, from
+    end
+    new_state = state.ready([token|ready])
+    new_state = new_state.players_by_token(players)
+    {:noreply, new_state}
+  end
+
+  def handle_call({:ready, token}, from,
+                  TableState[ready: ready,
+                             players_by_token: players]=state) when length(ready) == 3 do
+    if players == nil do
+      players = HashDict.new [{token, from}]
+    else
+      players = HashDict.put players, token, from
+    end
+    new_state = state.players_by_token(players)
+    send_you_turn(new_state)
+    {:noreply, new_state.ready([])}
+  end
+
+  def handle_call({:turn, token, card, hand}, _from,
+                  TableState[hands_by_token: hands,
+                             ids_by_token: ids,
+                             table: table]=state) do
+    case check_hand(card, hand) do
+      {:error, error} ->
+        {:reply, {:error, error}, state}
+      :ok ->
+        if hand != HashDict.get!(hands, token) do
+          {:reply, {:error, "Not your hand"}, state}
+        end
+
+        if List.member?(available_turns(hand, table), card) do
+          id = HashDict.get! ids, token
+          {new_hand, new_table} = turn(card, hand, id, table)
+
+          new_state = state.update_next_move(get_next_move &1)
+          new_state = new_state.hands_by_token(HashDict.put(hands, token, new_hand))
+          new_state = new_state.table(new_table)
+          send_you_turn(new_state)
+          {:reply, {:ok, {new_hand, new_table}}, new_state}
+        else
+          {:reply, {:error, "Unexpected move"}, state}
+        end
+    end
+  end
+
   def handle_call(_msg, _from, state) do
     IO.inspect(state)
     {:reply, {:error, "dont know what you mean"}, state}
+  end
+
+  # Private
+
+  defp send_you_turn(TableState[tokens_by_id: tokens,
+                                hands_by_token: hands,
+                                players_by_token: players,
+                                next_move: next_move,
+                                table: table]) do
+    next_player_token = HashDict.get!(tokens, next_move)
+
+    pid = HashDict.get!(players, next_player_token)
+    hand = HashDict.get!(hands, next_player_token)
+
+    :gen_server.reply(pid, {:you_turn, hand, table})
+  end
+
+  defp get_next_move(next_move) when next_move == 4 do
+    1
+  end
+  defp get_next_move(next_move) do
+    next_move + 1
   end
 
 end
--- test/table_test.exs
+++ test/table_test.exs
@@ -4,6 +4,17 @@ defmodule Kozel.Table.Test do
   use ExUnit.Case, async: true
 
   import Kozel.Table.Server
+  import Kozel.Cards, only: [ available_turns: 2 ]
+
+  defp receive_turn() do
+    receive do
+      {player, hand, table} ->
+        {player, hand, table}
+    after
+      5000 ->
+        assert false, "Timeout"
+    end
+  end
 
   setup do
     {:ok, pid} = :gen_server.start_link(Kozel.Table.Server, [], [])
@@ -18,10 +29,78 @@ defmodule Kozel.Table.Test do
     {:ok, token3} = :gen_server.call(server_pid, :join)
     {:ok, token4} = :gen_server.call(server_pid, :join)
 
-    assert Enum.count(:gen_server.call(server_pid, {:get_cards, token1})) == 8
-    assert Enum.count(:gen_server.call(server_pid, {:get_cards, token2})) == 8
-    assert Enum.count(:gen_server.call(server_pid, {:get_cards, token3})) == 8
-    assert Enum.count(:gen_server.call(server_pid, {:get_cards, token4})) == 8
+
+    hand1 = :gen_server.call(server_pid, {:get_cards, token1})
+    hand2 = :gen_server.call(server_pid, {:get_cards, token2})
+    hand3 = :gen_server.call(server_pid, {:get_cards, token3})
+    hand4 = :gen_server.call(server_pid, {:get_cards, token4})
+
+    assert Enum.count(hand1) == 8
+    assert Enum.count(hand2) == 8
+    assert Enum.count(hand3) == 8
+    assert Enum.count(hand4) == 8
+
+    self_pid = self
+
+    players = [:player1, :player2, :player3, :player4]
+
+    players_data = HashDict.new [{:player1, {token1, hand1}},
+                                 {:player2, {token2, hand2}},
+                                 {:player3, {token3, hand3}},
+                                 {:player4, {token4, hand4}}]
+
+    Process.spawn(fn() ->
+                      {:you_turn, hand, table} = :gen_server.call(server_pid,
+                                                                  {:ready, token1})
+                      self_pid <- {:player1, hand, table}
+                  end)
+    Process.spawn(fn() ->
+                      {:you_turn, hand, table} = :gen_server.call(server_pid,
+                                                                  {:ready, token2})
+                      self_pid <- {:player2, hand, table}
+                  end)
+    Process.spawn(fn() ->
+                      {:you_turn, hand, table} = :gen_server.call(server_pid,
+                                                                  {:ready, token3})
+                      self_pid <- {:player3, hand, table}
+                  end)
+    Process.spawn(fn() ->
+                      {:you_turn, hand, table} = :gen_server.call(server_pid,
+                                                                  {:ready, token4})
+                      self_pid <- {:player4, hand, table}
+                  end)
+
+    {player, hand, table} = receive_turn()
+
+    assert List.member?(players, player) == true
+    List.delete(players, player)
+
+    {token, chand} = HashDict.get!(players_data, player)
+    assert chand == hand
+    assert Enum.count(hand) == 8
+    assert Enum.count(table) == 0
+
+    [card|_] = available_turns(hand, table)
+    {:ok, {new_hand, new_table}} = :gen_server.call(server_pid, {:turn, token, card, hand})
+    assert Enum.count(new_hand) == 7
+    assert Enum.count(new_table) == 1
+
+    players_data = HashDict.put(players_data, player, {token, new_hand})
+
+    {player, hand, table} = receive_turn()
+
+    assert List.member?(players, player) == true
+    List.delete(players, player)
+
+    {token, chand} = HashDict.get!(players_data, player)
+    assert chand == hand
+    assert Enum.count(hand) == 8
+    assert Enum.count(table) == 1
+
+    [card|_] = available_turns(hand, table)
+    {:ok, {new_hand, new_table}} = :gen_server.call(server_pid, {:turn, token, card, hand})
+    assert Enum.count(new_hand) == 7
+    assert Enum.count(new_table) == 2
   end
 
 end
